3d8f2e0e132e5fa2dd9b84a734a486eaec2a06e2
komalgc <komalgc17@gmail.com>
2025-06-16 16:33:33 -0600

    Receipe1

---
+++ tests/Receipe1.spec.ts
@@ -0,0 +1,22 @@
+import {test, expect} from '@playwright/test';
+
+
+test('logintest', async({page}) =>{
+
+    await page.goto('https://bookcart.azurewebsites.net/login');
+    await page.getByPlaceholder('Username').fill('komalgc');
+    await page.getByPlaceholder('Password').fill('Treeboa@123');
+    await page.locator("mat-card-actions").getByRole("button", { name: "Login" }).click();
+    await page.waitForURL("https://bookcart.azurewebsites.net/");
+    await page.context().storageState({ path: "playwright/.auth.json" });
+
+})
+
+
+test.use({ storageState: 'playwright/.auth.json' });
+test ('Homepage test', async({page}) =>{
+
+    await page.goto('https://bookcart.azurewebsites.net/');
+    await expect(page.getByText('komalgc')).toBeVisible();
+
+})
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,15 +1,15 @@
 import {test, expect} from '@playwright/test';
 
+
 test('logintest', async({page}) =>{
 
     await page.goto('https://bookcart.azurewebsites.net/login');
     await page.getByPlaceholder('Username').fill('komalgc');
     await page.getByPlaceholder('Password').fill('Treeboa@123');
-    await page.getByRole('button', {name : 'Login'}).nth(1).click();
+    await page.locator("mat-card-actions").getByRole("button", { name: "Login" }).click();
     await page.waitForURL("https://bookcart.azurewebsites.net/");
-
- 
-  
-
+    
 
 })
+
+
