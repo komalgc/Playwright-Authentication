fc109c19eb0c902b60f5ca178cecea9227ee6d66
komalgc <komalgc17@gmail.com>
2025-06-17 11:32:38 -0600

    wishlist

--- tests/wishlist.spec.ts
+++ tests/wishlist.spec.ts
@@ -2,7 +2,7 @@ import {test, expect} from '@playwright/test'
 
 
 test.use({ storageState: 'playwright/.auth.json' });
-test ('Homepage test', async({page}) =>{
+test ('Wishlist test', async({page}) =>{
 
     await page.goto('https://bookcart.azurewebsites.net/');
     await expect(page.getByText('komalgc')).toBeVisible();
