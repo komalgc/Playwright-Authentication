7489d5d190059abd9539b4629390a9190c937f1e
komalgc <komalgc17@gmail.com>
2025-06-20 17:30:15 -0600

    Failed Recipe

--- tests/FailedRecipe3.spec.ts
+++ tests/FailedRecipe3.spec.ts
@@ -5,42 +5,34 @@ const loginpayload = { username: "buyerdev", password: "Treeboa@123" }
 let token;
 let orderid;
 
-
 test.beforeAll('apilogin', async () => {
 
     const apicontext = await request.newContext();
-
     const loginResponse = await apicontext.post('https://bookcart.azurewebsites.net/api/login',
-
         {
             data: loginpayload
         })
 
     expect(loginResponse.ok()).toBeTruthy();
-
     const loginResponseJson = await loginResponse.json();
     token = loginResponseJson.token;
     console.log(token)
 
     const orderresponse = await apicontext.get('https://bookcart.azurewebsites.net/api/Order/9961',
-
         {
-
             headers: {
-
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json'
             },
-
         }
     )
 
     expect(orderresponse.ok()).toBeTruthy();
     if (!orderresponse.ok()) {
-  const errorText = await orderresponse.text();
-  console.error("Failed to create order. Status:", orderresponse.status());
-  console.error("Response body:", errorText);
-}
+        const errorText = await orderresponse.text();
+        console.error("Failed to create order. Status:", orderresponse.status());
+        console.error("Response body:", errorText);
+    }
     const orderresponsejson = await orderresponse.json();
     console.log(orderresponsejson)
 
@@ -54,15 +46,20 @@ test('api test', async ({ page }) => {
         window.localStorage.setItem('token', value);
     }, token)
 
-
     await page.goto('https://bookcart.azurewebsites.net/')
+    await page.reload();
 
     console.log(await page.url());
+    const storedToken = await page.evaluate(() => {
+        return window.localStorage.getItem('token');
+    });
 
+    console.log("✅ Token in localStorage:", storedToken);
     await page.goto('https://bookcart.azurewebsites.net/myorders')
-    const orderID = await page.getByRole('row').locator('tr/td').nth(3).textContent();
-
+    const tokenInBrowser = await page.evaluate(() => localStorage.getItem('token'));
+    await expect(tokenInBrowser).toBe(token);
+    await page.waitForLoadState('networkidle');
+    const orderID = await page.locator('tbody tr').first().locator('td').nth(1).textContent();
     console.log(orderID)
-
     expect(orderID).toContain(orderid);
 })
