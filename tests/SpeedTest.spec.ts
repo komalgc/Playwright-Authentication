1d02efdd69f39dd4f8f6dca4ff85cc56bf254f3a
komalgc <komalgc17@gmail.com>
2025-06-23 19:24:10 -0600

    speedtest

---
+++ tests/SpeedTest.spec.ts
@@ -0,0 +1,24 @@
+import {test, expect} from '@playwright/test';
+
+
+let bookscount ;
+test('Homepage test', async({page}) =>{
+
+    await page.goto('https://bookcart.azurewebsites.net/');
+    await expect(page.getByText('account_circlearrow_drop_down')).toBeVisible();
+    
+
+})
+
+test ('Wishlist test', async({page}) =>{
+
+    await page.goto('https://bookcart.azurewebsites.net/');
+    await expect(page.getByText('komalgc')).toBeVisible();
+    await page.getByRole('button').filter({ hasText: 'favorite' }).click();
+    await expect(page).toHaveURL('https://bookcart.azurewebsites.net/wishlist');
+    bookscount = await page.locator('tr').count(); 
+    const actualcount = bookscount - 1;
+    console.log(actualcount);
+
+})
+
