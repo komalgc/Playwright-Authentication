2cc5b3c57f69602dbfd89c3eb520d937fe1b0526
komalgc <komalgc17@gmail.com>
2025-06-24 16:18:19 -0600

    SSO test

--- tests/sso.spec.ts
+++ tests/sso.spec.ts
@@ -1,14 +1,14 @@
 import { test } from '@playwright/test'
 import 'dotenv/config';
 
-test('Google login', async ({ page }) => {
+test('SSO -Google login', async ({ page }) => {
   await page.goto('https://stackoverflow.com/users/login')
 
   await page.getByRole('button', { name: 'Log in with Google' }).click()
-  await page.getByLabel('Email or phone').fill("buyerdev99@gmail.com")
+  await page.getByLabel('Email or phone').fill(process.env.GOOGLE_USER ?? "")
   await page.getByRole('button', { name: 'Next' }).click()
 
-  await page.getByLabel('Enter your password').fill("Treeboa@123")
+  await page.getByLabel('Enter your password').fill(process.env.GOOGLE_PWD ?? "")
   await page.getByRole('button', { name: 'Next' }).click();
     await page.getByRole('button', { name: 'Accept all cookies' }).click();
 
