ddf4ee03d6ab3dc9f89f8e23edfc6d2cdec844fd
komalgc <komalgc17@gmail.com>
2025-06-24 16:28:55 -0600

    SSO login Recipe7

---
+++ tests/Recipe7.spec.ts
@@ -0,0 +1,15 @@
+import { test } from '@playwright/test'
+import 'dotenv/config';
+
+test('SSO -Google login', async ({ page }) => {
+  await page.goto('https://stackoverflow.com/users/login')
+
+  await page.getByRole('button', { name: 'Log in with Google' }).click()
+  await page.getByLabel('Email or phone').fill(process.env.GOOGLE_USER ?? "")
+  await page.getByRole('button', { name: 'Next' }).click()
+
+  await page.getByLabel('Enter your password').fill(process.env.GOOGLE_PWD ?? "")
+  await page.getByRole('button', { name: 'Next' }).click();
+    await page.getByRole('button', { name: 'Accept all cookies' }).click();
+
+})
