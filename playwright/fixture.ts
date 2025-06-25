c526daee17fbebefa9f2e511e7aa6afde0ec31dd
komalgc <komalgc17@gmail.com>
2025-06-24 23:34:15 -0600

    added wait for url

--- playwright/fixture.ts
+++ playwright/fixture.ts
@@ -5,57 +5,55 @@ import 'dotenv/config';
 
 
 let account: { username: any; password: any };
-  // Define a function to return account credentials based on the ID
+// Define a function to return account credentials based on the ID
 async function acquireAccount(id: number) {
- 
+
   // You can have a list of pre-created accounts for testing.
- if (id === 0) {
-   account = {
-     username: process.env.username0,
-     password: process.env.password0,
-   };
- } else if (id === 1) {
-   account = {
-     username: process.env.username1,
-     password: process.env.password1,
-   };
- }
- return account;
+  if (id === 0) {
+    account = {
+      username: process.env.username0,
+      password: process.env.password0,
+    };
+  } else if (id === 1) {
+    account = {
+      username: process.env.username1,
+      password: process.env.password1,
+    };
+  }
+  return account;
 }
 
 export const test = baseTest.extend<{}, { workerStorageState: string }>({
 
   // Define a fixture to provide the page object with authentication state.
- storageState: ({ workerStorageState }, use) => use(workerStorageState),
- // This fixture will be used to authenticate the user and store the state.
- workerStorageState: [
-   async ({ browser }, use) => {
-     // Use parallelIndex as a unique identifier for each worker.
-     const id = test.info().parallelIndex;
-     const fileName = `playwright/.auth${id}.json`;
+  storageState: ({ workerStorageState }, use) => use(workerStorageState),
+  // This fixture will be used to authenticate the user and store the state.
+  workerStorageState: [
+    async ({ browser }, use) => {
+      // Use parallelIndex as a unique identifier for each worker.
+      const id = test.info().parallelIndex;
+      const fileName = `playwright/.auth${id}.json`;
 
-     if (fs.existsSync(fileName)) {
-     
-       await use(fileName);
-       return;
-     }
+      if (fs.existsSync(fileName)) {
 
-     const page = await browser.newPage({ storageState: undefined });
-     account = await acquireAccount(id);
-     
-     await page.goto("https://bookcart.azurewebsites.net/");
-     await page.getByRole("button", { name: " Login " }).click();
-     await page.getByLabel("Username").fill(account.username);
-     await page.getByLabel("Password").fill(account.password);
-     await page
-       .locator("mat-card-actions")
-       .getByRole("button", { name: "Login" }).click();
+        await use(fileName);
+        return;
+      }
 
-     await page.waitForURL("https://bookcart.azurewebsites.net/");
-   
-     await page.context().storageState({ path: `playwright/.auth${id}.json` });
-     await page.close();
-     await use(fileName);},
-   { scope: "worker" },
- ],
+      const page = await browser.newPage({ storageState: undefined });
+      account = await acquireAccount(id);
+
+      await page.goto("https://bookcart.azurewebsites.net/");
+      await page.getByRole("button", { name: " Login " }).click();
+      await page.getByLabel("Username").fill(account.username);
+      await page.getByLabel("Password").fill(account.password);
+      await page.locator("mat-card-actions").getByRole("button", { name: "Login" }).click();
+      await page.waitForURL("https://bookcart.azurewebsites.net/");
+
+      await page.context().storageState({ path: fileName });
+      await page.close();
+      await use(fileName);
+    },
+    { scope: "worker" },
+  ],
 });  
--- tests/Recipe2.spec.ts
+++ tests/Recipe2.spec.ts
@@ -6,26 +6,46 @@ import { expect } from '@playwright/test'
 
 
 let bookscount;
-test.only('first test account 1', async ({ page }) => {
+test('first test account 1', async ({ page }) => {
 
+  // Navigate to the login page
   await page.goto('https://bookcart.azurewebsites.net/');
   let rawText = await page.getByText('account_circlearrow_drop_down').textContent();
   const username = rawText ? rawText.split(' ').pop()?.trim() : '';
+
+  //Verify the user is logged in
   console.log('The logged in User is ' + username);
+
+  //go to the wishlist page
   await page.getByRole('button').filter({ hasText: 'favorite' }).click();
+  await page.waitForURL("https://bookcart.azurewebsites.net/wishlist");
   await expect(page).toHaveURL('https://bookcart.azurewebsites.net/wishlist');
 
+  //Verify the wishlist page is not empty
   bookscount = await page.locator('tr').count();
   const actualcount = bookscount - 1;
-  console.log('The no of books in wishlist is ' +actualcount);
+  console.log('The no of books in wishlist is ' + actualcount);
 
 })
 test("second test account 2", async ({ page }) => {
+
+  // Navigate to the login page
   await page.goto("https://bookcart.azurewebsites.net/");
   await page.waitForURL("https://bookcart.azurewebsites.net/");
+
+
+  let rawText = await page.getByText('account_circlearrow_drop_down').textContent();
+  const username = rawText ? rawText.split(' ').pop()?.trim() : '';
+
+  //Verify the user is logged in
+  console.log('The logged in User is ' + username);
+
+  //Verify the wishlist page is  empty
   await page.getByRole('button').filter({ hasText: 'favorite' }).click();
+  await page.waitForURL("https://bookcart.azurewebsites.net/wishlist");
   await expect(page).toHaveURL('https://bookcart.azurewebsites.net/wishlist');
   await expect(page.getByText('Your wishlist is empty.')).toBeVisible();
+  console.log('Your wishlist is empty.');
 
 });
 
