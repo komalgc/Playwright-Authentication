8c6d5c1868c10616a3fd4cd5522249d8690656a5
komalgc <komalgc17@gmail.com>
2025-06-25 13:11:26 -0600

    sso login

--- tests/Recipe7.spec.ts
+++ tests/Recipe7.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import 'dotenv/config';
 
 // 👩‍💻📱 🔐 Authenticate for multiple accounts via the API and use
@@ -10,7 +10,7 @@ import 'dotenv/config';
 
 test('SSO -Google login', async ({ page }) => {
   await page.goto('https://stackoverflow.com/users/login')
-
+  //redirect to Google login page
   await page.getByRole('button', { name: 'Log in with Google' }).click()
   await page.getByLabel('Email or phone').fill(process.env.GOOGLE_USER ?? "")
   await page.getByRole('button', { name: 'Next' }).click()
@@ -18,5 +18,6 @@ test('SSO -Google login', async ({ page }) => {
   await page.getByLabel('Enter your password').fill(process.env.GOOGLE_PWD ?? "")
   await page.getByRole('button', { name: 'Next' }).click();
   await page.getByRole('button', { name: 'Accept all cookies' }).click();
-
+  await page.waitForURL('https://stackoverflow.com/');
+  await expect(page.getByText('Welcome to Stack Overflow, buyerdev99!')).toBeVisible();
 })
